a3ed7c23e2a41250a4beb8765a7b1c77ad03a887
Md Shakil Hossain <40922031+mdShakilHossainNsu2018@users.noreply.github.com>
2020-09-05 13:47:25 +0600

    h b1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,141 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <v-app id="keep">
+        <v-app-bar
+                app
+                clipped-left
+                color="amber"
+        >
+            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+            <span class="title ml-3 mr-5">Kdp&nbsp;<span class="font-weight-light">App</span></span>
+            <v-text-field
+                    solo-inverted
+                    flat
+                    hide-details
+                    label="Search"
+                    prepend-inner-icon="mid-magnifier"
+            ></v-text-field>
+
+            <v-spacer></v-spacer>
+        </v-app-bar>
+
+        <v-navigation-drawer
+                v-model="drawer"
+                app
+                clipped
+                color="grey lighten-4"
+        >
+            <v-list
+                    dense
+                    class="grey lighten-4"
+            >
+                <template v-for="(item, i) in items">
+                    <v-row
+                            v-if="item.heading"
+                            :key="i"
+                            align="center"
+                    >
+                        <v-col cols="6">
+                            <v-subheader v-if="item.heading">
+                                {{ item.heading }}
+                            </v-subheader>
+                        </v-col>
+                        <v-col
+                                cols="6"
+                                class="text-right"
+                        >
+                            <v-btn
+                                    small
+                                    text
+                            >edit</v-btn>
+                        </v-col>
+                    </v-row>
+                    <v-divider
+                            v-else-if="item.divider"
+                            :key="i"
+                            dark
+                            class="my-4"
+                    ></v-divider>
+                    <v-list-item
+                            v-else
+                            :key="i"
+                            link
+                    >
+                        <v-list-item-action>
+                            <v-icon>mid-{{ item.icon }}</v-icon>
+                        </v-list-item-action>
+                        <v-list-item-content>
+                            <v-list-item-title class="grey--text">
+                                {{ item.text }}
+                            </v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </template>
+            </v-list>
+        </v-navigation-drawer>
+
+        <v-main>
+            <v-container
+                    fluid
+                    class="grey lighten-4 fill-height"
+            >
+                <v-row
+                        justify="center"
+                        align="center"
+                >
+                    <v-col >
+                        <Index/>
+<!--                        <v-tooltip right>-->
+<!--                            <template>-->
+<!--&lt;!&ndash;                                <v-btn&ndash;&gt;-->
+<!--&lt;!&ndash;                                        :href="source"&ndash;&gt;-->
+<!--&lt;!&ndash;                                        icon&ndash;&gt;-->
+<!--&lt;!&ndash;                                        large&ndash;&gt;-->
+<!--&lt;!&ndash;                                        target="_blank"&ndash;&gt;-->
+<!--&lt;!&ndash;                                        v-on="on"&ndash;&gt;-->
+<!--&lt;!&ndash;                                >&ndash;&gt;-->
+<!--&lt;!&ndash;                                    <v-icon large>mdi-code-tags</v-icon>&ndash;&gt;-->
+<!--&lt;!&ndash;                                </v-btn>&ndash;&gt;-->
+<!--                            </template>-->
+<!--                            <span>Source</span>-->
+<!--                        </v-tooltip>-->
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-main>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    import Index from "@/components/Index";
+    export default {
+        components: {Index},
+        props: {
+            source: String,
+        },
+        data: () => ({
+            drawer: null,
+            items: [
+                { icon: 'lightbulb_outline', text: 'Notes' },
+                { icon: 'touch_app', text: 'Reminders' },
+                { divider: true },
+                { heading: 'Labels' },
+                { icon: 'add', text: 'Create new label' },
+                { divider: true },
+                { icon: 'archive', text: 'Archive' },
+                { icon: 'delete', text: 'Trash' },
+                { divider: true },
+                { icon: 'settings', text: 'Settings' },
+                { icon: 'chat_bubble', text: 'Trash' },
+                { icon: 'help', text: 'Help' },
+                { icon: 'phonelink', text: 'App downloads' },
+                { icon: 'keyboard', text: 'Keyboard shortcuts' },
+            ],
+        }),
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #keep .v-navigation-drawer__border {
+        display: none
+    }
 </style>
--- src/components/HtmlEditor.vue
+++ src/components/HtmlEditor.vue
@@ -1,10 +1,66 @@
 <template>
-    $END$
+    <div >
+
+
+
+        <v-row justify="left">
+            <v-dialog v-model="dialog" width="600px">
+                <template v-slot:activator="{ on, attrs }">
+                    <v-row>
+                        <v-col>
+                            <vue-editor v-model="content"></vue-editor>
+                            <v-btn class="mt-2"
+                                    color="primary"
+                                    dark
+                                    v-bind="attrs"
+                                    v-on="on"
+
+                            >
+                                Generate Html
+                            </v-btn>
+                        </v-col>
+                    </v-row>
+
+                </template>
+                <v-card>
+                    <v-card-title>
+                        <span class="headline">Copy the Html</span>
+                    </v-card-title>
+                    <v-card-text>{{content}}</v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="green darken-1" text @click="dialog = false">Close</v-btn>
+                        <v-btn color="green darken-1"  v-clipboard:copy="content" text @click="dialog = false">Copy</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+        </v-row>
+
+    </div>
 </template>
 
 <script>
+    import { VueEditor } from "vue2-editor";
     export default {
-        name: "HtmlEditor"
+        name: "HtmlEditor",
+
+        components: {
+            VueEditor
+        },
+
+        methods: {
+            click(){
+                console.log(this.content);
+            }
+        },
+
+        data() {
+            return {
+                content: "",
+                dialog: false,
+            };
+        }
+
     }
 </script>
 
